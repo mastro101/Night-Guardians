ef50b4dde18d36069bc70e808934a57985312a5d
AndreaAncona <a.ancona.bb@gmail.com>
2018-07-13 12:57:04 +0200

    nuovi assets

---
+++ Assets/Dropzone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dropzone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public int childcount;
+
+    public void Update()
+    {
+        childcount = gameObject.transform.childCount;
+    }
+
+
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log("Ondrop to ");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Ondrop to ");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log (eventData.pointerDrag.name + " was dropped on " + gameObject.name); 
+    }
+}
---
+++ Assets/Script/attackzones.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class attackzones : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public int childnumber;
+    public bool ciao;
+
+    
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerEnter");
+        if (eventData.pointerDrag == null)
+            return;
+
+        if (childnumber >= 1)
+            return;
+
+
+        cardsbeh d = eventData.pointerDrag.GetComponent<cardsbeh>();
+        if (d != null)
+        {
+            d.placeholderParent = this.transform;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerExit");
+        if (eventData.pointerDrag == null)
+            return;
+
+        if (childnumber >= 1)
+            return;
+
+
+        cardsbeh d = eventData.pointerDrag.GetComponent<cardsbeh>();
+        if (d != null && d.placeholderParent == this.transform)
+        {
+            d.placeholderParent = d.parentToReturnTo;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+        if (ciao == true)
+            return;
+ 
+
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        cardsbeh d = eventData.pointerDrag.GetComponent<cardsbeh>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+
+    }
+
+    public void LateUpdate()
+
+    {
+
+        childnumber = transform.childCount;
+        if (childnumber == 0 && ciao == true)
+
+            ciao = false;
+    }
+
+}
