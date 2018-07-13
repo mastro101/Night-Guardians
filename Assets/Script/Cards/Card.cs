239b0e5fd7a6efb328d334031d7ee761cf33a447
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-07-13 20:08:10 +0200

    Creato ScriptableObject per le carte + classe Card

---
+++ Assets/Script/Cards/Card.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+
+    public CardsData Data;
+    [HideInInspector]
+    public CardType Type;
+    [HideInInspector]
+    public int Attack;
+    int life;
+    public int Life
+    {
+        get
+        { return life; }
+        set
+        {
+            life = value;
+            lifeText.text = life.ToString();
+            if (life <= 0)
+            {
+                Debug.Log(Data.Name + " è morto");
+            }
+        }
+    }
+
+    int purificationOrDarkness;
+    public int PurificationOrDarkness
+    {
+        get { return purificationOrDarkness; }
+        set
+        {
+            purificationOrDarkness = value;
+        }
+    }
+
+
+    [SerializeField]
+    Image imageCard, imageCover;
+    [SerializeField]
+    Text nameText, attackText, lifeText, purificationOrDarknessText, descriptionText;
+    
+    private void Start()
+    {
+        Type = Data.Type;
+        Attack = Data.Attack;
+        Life = Data.Life;
+        PurificationOrDarkness = Data.PurificationOrDarkness;
+        nameText.text = Data.Name;
+        imageCard.sprite = Data.SpriteImage;
+        attackText.text = Data.Attack.ToString();
+        lifeText.text = Data.Life.ToString();
+        purificationOrDarknessText.text = Data.PurificationOrDarkness.ToString();
+        descriptionText.text = Data.Description;
+        if (Type == CardType.Guardian)
+        {
+            nameText.color = Color.black;
+            descriptionText.color = Color.black;
+            attackText.color = Color.black;
+            lifeText.color = Color.black;
+            purificationOrDarknessText.color = Color.black;
+            imageCover.color = Color.white;
+        }
+        else if (Type == CardType.Nightmare)
+        {
+            nameText.color = Color.white;
+            descriptionText.color = Color.white;
+            attackText.color = Color.white;
+            lifeText.color = Color.white;
+            purificationOrDarknessText.color = Color.white;
+            imageCover.color = Color.black;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Life--;
+        }
+    }
+}
---
+++ Assets/Script/Cards/CardsData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card", order = 0)]
+public class CardsData : ScriptableObject {
+
+    public CardType Type;
+    public string Name;
+    public int Attack;
+    public int Life;
+    public int PurificationOrDarkness;
+    public string Description;
+    public Sprite SpriteImage;
+}
+
+public enum CardType
+{
+    Guardian,
+    Nightmare,
+}
