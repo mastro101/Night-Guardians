6775f6b02b5ce9a99625ff38d740a3967bff5d90
AndreaAncona <a.ancona.bb@gmail.com>
2018-07-13 13:20:27 +0200

    d

--- Assets/Dropzone.cs
+++ Assets/Dropzone.cs
@@ -1,29 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.EventSystems;
 
-public class Dropzone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
 
-    public int childcount;
+	public void OnPointerEnter(PointerEventData eventData) {
+		//Debug.Log("OnPointerEnter");
+		if(eventData.pointerDrag == null)
+			return;
 
-    public void Update()
-    {
-        childcount = gameObject.transform.childCount;
-    }
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if(d != null) {
+			d.placeholderParent = this.transform;
+		}
+	}
+	
+	public void OnPointerExit(PointerEventData eventData) {
+		//Debug.Log("OnPointerExit");
+		if(eventData.pointerDrag == null)
+			return;
 
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if(d != null && d.placeholderParent==this.transform) {
+			d.placeholderParent = d.parentToReturnTo;
+		}
+	}
+	
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log (eventData.pointerDrag.name + " was dropped on " + gameObject.name);
 
-    public void OnDrop(PointerEventData eventData) {
-        Debug.Log("Ondrop to ");
-    }
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if(d != null) {
+			d.parentToReturnTo = this.transform;
+		}
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("Ondrop to ");
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        Debug.Log (eventData.pointerDrag.name + " was dropped on " + gameObject.name); 
-    }
+	}
 }
