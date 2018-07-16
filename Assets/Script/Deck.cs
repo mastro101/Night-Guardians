8b674a4c9556e7c92449cd93e3927890405e3b3b
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-07-16 14:27:37 +0200

    Creazione Deck provvisorio

---
+++ Assets/Script/Deck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+
+    [SerializeField]
+    CardsData[] Cards;
+    [SerializeField]
+    DropZone Hand;
+    [SerializeField]
+    GameObject card;
+
+    public void Draw()
+    {
+        Instantiate(card, Hand.transform);
+        card.GetComponent<Card>().Data = Cards[Random.Range(0, Cards.Length + 1)];
+    }
+}
