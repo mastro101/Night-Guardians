4c845e758d5e1d67bad358022ea68d81d3b8a4f8
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-07-26 14:04:29 +0200

    Menu iniziale e menu Esci dal gioco

---
+++ Assets/Script/Menu/QuitGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+    [SerializeField]
+    GameObject quitMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!quitMenu.activeInHierarchy)
+                quitMenu.SetActive(true);
+            else
+                quitMenu.SetActive(false);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void CloseMenu()
+    {
+        quitMenu.SetActive(false);
+    }
+}
