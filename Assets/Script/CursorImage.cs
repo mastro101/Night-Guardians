337119d3742ce6696517e6e6c383fc2241bf7db0
AndreaAncona <a.ancona.bb@gmail.com>
2018-07-30 13:41:20 +0200

    cose estetiche ma ti conviene cominciare ad usare questo per lavorarci

---
+++ Assets/Script/CursorImage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorImage : MonoBehaviour {
+
+    public Texture2D cursorImage;
+
+
+	// Use this for initialization
+	void Start () {
+
+        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.Auto);
+
+	}
+
+}
+
