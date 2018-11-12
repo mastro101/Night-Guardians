5eafd8ddf91a6e915e4d2772da2ff12cb767cba5
Galahad91 <alex.elia.91@gmail.com>
2018-11-12 16:13:51 +0100

    implemented card effect class

---
+++ Assets/Script/Cards/CardEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CardEffect : MonoBehaviour
+{
+    protected Card card;
+
+    protected virtual void Awake()
+    {
+        card = GetComponent<Card>();
+    }
+
+    public virtual void ApplyEffect()
+    { }
+    public virtual void ApplyEffect(int value)
+    { }
+}
+
---
+++ Assets/Script/Cards/Regeneration.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Regeneration : CardEffect
+{
+    public override void ApplyEffect()
+    {
+        if(card.IsAlive)
+        {
+            card.Life = card.Data.Life;
+        }
+    }
+}
